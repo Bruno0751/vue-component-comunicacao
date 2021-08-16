9c3335a05242e8c95cbfd4fdbc0db4569762b7cb
bruno.silveira <brunosilveira@gentecontrole.com.br>
2021-08-16 18:14:07 -0300

    update

--- src/components/FilmesLista.vue
+++ src/components/FilmesLista.vue
@@ -6,19 +6,22 @@
 
       <h2>Filmes</h2>
 
-      <div class="list-group list-group-flush">
+      <ul class="list-group list-group-flush">
 
-        <FilmesListaIten filmeTitulo="Viuva Negra" />
-        <FilmesListaIten filmeTitulo="Homem Aranha de Volta ao Lar" />
-        <FilmesListaIten filmeTitulo="Homem de Ferro" />
+        <FilmesListaIten
+          v-for="filme in filmes"
+          :key="filme.id"
+          :filme="filme"
+          @selecionarFilme="filmeSelecionado = $event"
+        />
 
-      </div>
+      </ul>
     </div>
 
     <!-- coluna 2 -->
     <div class="col-4">
 
-      <FilmesListaItenInfo/>
+      <FilmesListaItenInfo />
 
     </div>
 
@@ -34,6 +37,16 @@ export default {
   components: {
     FilmesListaIten,
     FilmesListaItenInfo
-  }
+  },
+  data () {
+    return {
+      filmes: [
+        {id: 1, titulo: 'Viuva Negra', ano: 2021},
+        {id: 2, titulo: 'Homem Aranha de Volta ao Lar', ano: 2019},
+        {id: 3, titulo: 'Homem de Ferro', ano: 2001},
+      ],
+      filmeSelecionado: undefined,
+    }
+  },
 }
 </script>
